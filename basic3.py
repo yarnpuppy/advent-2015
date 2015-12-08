ad8874b8076e50b66b180b84e7e0ed816edd6695
yarnpuppy <mullings@Marjas-iMac.local>
2015-12-08 16:15:25 -0600

    removed some comments

--- basic3.py
+++ basic3.py
@@ -44,38 +44,4 @@ print scoordinate
 
 
 
-#with open("basic3.txt") as f:
-	#for line in f:
-
-		
-
-
-	#scoordinate = scoordinate + scoordinate.append((up, right))
-	#print scoordinate
-
-		
-		
-
-
-		#"^" = [1,0]
-		#print [^]
-		
-		#"v" == -1*"^"
-
-		#print 
-
-		#"v" ==-1*""^""
-		#east == [0,1]
-		#'>' == east
-		
-		#west == -1*[0,1]
-		#'<' = -1* '>' 
-
-	#scoordinate = scoordinate + scoordinate.append((north, south))
-
-#print [scoordinate]
-
-
-
-
 
